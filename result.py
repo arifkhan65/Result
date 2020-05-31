d5a02c3bc0ffc466b214a3be72a35c42c00b6db8
Arif <arifkhan65@gmail.com>
2020-05-31 03:36:56 +0200

    Initial commit

---
+++ result.py
@@ -0,0 +1,33 @@
+
+print('input marks in math: ')
+math=input()
+if math > '060':
+    print('you got first division marks in Math')
+elif math >= '050':
+    print('you got Second division marks in Math')
+elif math >='040':
+    print('you got third division marks in Math')
+elif math < '040':
+    print('you failed in Math')
+
+print('input marks in Physics: ')
+Physics=input()
+if Physics > '060':
+    print('you got first division marks in Physics')
+elif Physics >= '050':
+    print('you got Second division marks in Physics')
+elif Physics >='040':
+    print('you got third division marks in Physics')
+elif Physics < '040':
+    print('you failed in Physics')
+
+print('input marks in english: ')
+english=input()
+if english > '060':
+    print('you got first division marks in english')
+elif english >= '050':
+    print('you got Second division marks in english')
+elif english >='040':
+    print('you got third division marks in english')
+elif english < '040':
+    print('you failed in english')
